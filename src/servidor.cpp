d990a406e3c8cf93fd20119a8d59a9b3af97df6b
mutska <mutska@ciencias.unam.mx>
2018-08-28 15:23:21 -0500

    Archivo del servidor inicializado

---
+++ src/servidor.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+int main(){
+
+
+  return 0;
+}
