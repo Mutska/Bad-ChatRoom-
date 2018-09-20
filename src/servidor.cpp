8052a18795a0ca02ed7abefe2e798caafc89d8af
mutska <mutska@ciencias.unam.mx>
2018-09-20 01:06:42 -0500

    Servidor renovado, para que los mensajes recibidos sean tratados como strings :)

--- src/servidor.cpp
+++ src/servidor.cpp
@@ -14,6 +14,9 @@
 #include <map>
 #include "Usuario.h"
 #include "Auxiliares.h"
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 Usuario* temporal;
 map<string,Usuario*> list;
@@ -47,8 +50,8 @@ int main(){
   
   //Configuramos las estructura del servidor 
   server_info.sin_family = AF_INET;
-  server_info.sin_addr.s_addr = inet_addr("192.168.15.8");
-  server_info.sin_port = htons(8080);
+  server_info.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server_info.sin_port = htons(1234);
 
   //Hacemos un bind para reservar el puerto que pensamos ocupar para el servidor
   bind(server_socket,(struct sockaddr *)&server_info,server_space);
@@ -65,35 +68,48 @@ int main(){
   //Variables necesarias para probar la veracidad el comando que el cliente conectado introduce
   size_t found;
   string name;
+  string real;
   string need = "IDENTIFY ";
   char error[256] = "EL comando que introduciste es incorrecto vuelve a conectarde.. adios :V";
+  int band = 0;
   while(1){
     //Si hay conexion se acepta
     client_socket = accept(server_socket,(struct sockaddr*) &client_info, (socklen_t*) &client_space);
+    
     // Imprime el ip del cliente que entra
     getpeername(client_socket, (struct sockaddr*) &client_info, (socklen_t*) &client_space);
     cout<<"Cliente "<<inet_ntoa(client_info.sin_addr)<<":"<<ntohs(client_info.sin_port)<<"."<<"Entrando"<<endl;
     
     //Y se envia un mensaje al cliente
     send(client_socket,msg,sizeof(msg),0);
+    memset(client_response,0,sizeof(client_response));
     
     //Se recibe un mensaje del cliente
     recv(client_socket,&client_response,sizeof(client_response),0);
     stream.str(client_response);
     cadena  = stream.str();
     found = cadena.find(need);
+    real.clear();
     if(found != std::string::npos){
       name = cadena.substr(found+9);
-      cout<<"El cliente envio su nombre: "<<name<<endl;
-    }else{
+      name.erase(remove(name.begin(), name.end(), ' '), name.end());
+      for(int i = 0;i<name.size();i++){
+	if(isalpha(name[i]))
+	  real += name[i];	
+      }
+      cout<<"El cliente envio su nombre: "<<real<<endl;
+      band = 0;
+    }else if(found == std::string::npos){
       send(client_socket,error,sizeof(error),0);
-      cout<<"Servidor cerrandose ante agresividad del cliente :("<<endl;
-      break;
+      cout<<"Cliente no aceptado :("<<endl;
+      close(client_socket);
+      band = 1;
     }
-    name = cadena.substr(found+9);
+    if(band)
+      continue;
     //guardamos el nuevo cliente en el diccionario si es que pasa la prueba del nombre
-    temporal = newNode(client_socket, inet_ntoa(client_info.sin_addr),name);
-    list[name] = temporal;
+    temporal = newNode(client_socket, inet_ntoa(client_info.sin_addr),real);
+    list[real] = temporal;
     
     pthread_t server_thread;
     if (pthread_create(&server_thread, NULL, (void *)manager, (void *)temporal) != 0) {
@@ -103,6 +119,7 @@ int main(){
     
   }
   //Cerrar el servidor
+  close(client_socket);
   close(server_socket);
   return 0;
 }
@@ -122,30 +139,64 @@ void get_out(int sig) {
 
 //Manejador del cliente 
 void manager(void *client) {   //el parametro p_client es la direccion de p_client por eso le precede un puntero void
-  int flag = 0;
-  char nickname[50] = {};
-  char recv_buffer[250000] = {};
-  char send_buffer[250000] = {};
   Usuario *cliente = (Usuario *)client;
+  int flag = 0;
+  int test;
+  int tam;
+  int pivote;
+  int roomname_len;
+  char recv_buffer[250000] = {};
+  string send_buffer = "DEFAULT";
+  string cadena;
+  string status;
+  string roomname;
+  string case_five;
+  string roomname_message;
+  stringstream stream;
+  vector <string> case_seven;
     // Ciclo  que esta pendiente a las solicitudes del cliente, esta sera la funcion mas importante del servidor
   int receive;
   while (1) {
     if (flag) {//si esta activada la bandera el servidor cierra
       break;
     }
-    receive = recv(cliente->data, recv_buffer,1000, 0);
+    memset(recv_buffer,0,sizeof(recv_buffer));
+    int receive = recv(cliente->data, recv_buffer,1000, 0);
     if (receive > 0) {
       if (strlen(recv_buffer) == 0) {
 	continue;
       }
-      sprintf(nickname, "%s" ,cliente->name.c_str());
-      trimm(nickname, 50);
-      strcpy(send_buffer,"");
-      sprintf(send_buffer, "%s：%s" , nickname, recv_buffer);
+      cadena.clear();
+      send_buffer.clear();
+      cout<<"LA CADENA EN EL PRINCIPIO DE LOS TIEMPOS  CONTIENE: ["<<send_buffer<<"]"<<endl;
+      stream.str(recv_buffer);
+      cadena  = stream.str();
+      tam = cadena.size();
+      cout<<"El largo de la cadena es: "<<tam<<endl;
+      cout<<"La cadena recibida es la siguiente: "<<cadena<<endl;
+      test = detect(cadena);
+      cadena.erase(std::remove(cadena.begin(), cadena.end(), '\n'), cadena.end());
+      if(test != std::string::npos){
+	if(test == 1){
+	  send_buffer = send_arguments(1,cadena);
+	  send_buffer += "\n";
+	  
+	}
+	
+      }
+      
+
+      
     } else if (receive == 0) {
       cout<<cliente->name<<"("<<cliente->ip<<")"<<"("<<cliente->data<<")"<<" dejo el chat"<<endl;
-      sprintf(send_buffer, "%s(%s) dejo el chat.", nickname, cliente->ip);
+      send_buffer = cliente->name;
+      send_buffer += "(";
+      send_buffer += cliente->ip;
+      send_buffer += ") ";
+      send_buffer += "dejo el chat\n";
+      send_buffer += "\n";
       flag = 1;
+      
     } else {
       cout<<"Error :("<<endl;
       flag = 1;
